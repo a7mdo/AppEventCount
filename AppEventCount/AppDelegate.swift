87e34f7daa35a2f000161e6b6286320609157d4a
Ahmed Naser <201700099@student.polytechnic.bh>
2020-11-19 04:14:19 +0300

    Implement

--- AppEventCount/AppDelegate.swift
+++ AppEventCount/AppDelegate.swift
@@ -10,7 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    //var window: UIWindow? // was not here in the 1st place, I added it manually but still didn't work
+    var viewController: ViewController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,7 +31,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        //viewController = window?.rootViewController as? ViewController // Won't compile since window not declared in the scope
+        
+        viewController?.didFinishLaunchingCount += 1
+    }
 
-
+    func applicationWillResignActive(_ application: UIApplication) {
+        viewController?.willResignActiveCount += 1
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        viewController?.didEnterBackgroundCount += 1
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        viewController?.willEnterForegroundCount += 1
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        viewController?.didBecomeActiveCount += 1
+        viewController?.updateView()
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        viewController?.willTerminateCount += 1
+    }
 }
 
--- AppEventCount/ViewController.swift
+++ AppEventCount/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var didFinishLaunchingLabel: UILabel!
+    @IBOutlet weak var willResignActiveLabel: UILabel!
+    @IBOutlet weak var didEnterBackgroundLabel: UILabel!
+    @IBOutlet weak var willEnterForegroundLabel: UILabel!
+    @IBOutlet weak var didBecomeActiveLabel: UILabel!
+    @IBOutlet weak var willTerminateLabel: UILabel!
+    
+    var didFinishLaunchingCount = 0
+    var willResignActiveCount = 0
+    var didEnterBackgroundCount = 0
+    var willEnterForegroundCount = 0
+    var didBecomeActiveCount = 0
+    var willTerminateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateView()
     }
 
-
+    func updateView() {
+        didFinishLaunchingLabel.text = "The app has launched \(didFinishLaunchingCount) time(s)"
+        willResignActiveLabel.text = "The app has resign active \(willResignActiveCount) time(s)"
+        didEnterBackgroundLabel.text = "The app has entered background \(didEnterBackgroundCount) time(s)"
+        
+        willEnterForegroundLabel.text = "The app has entered foreground \(willEnterForegroundCount) time(s)"
+        didBecomeActiveLabel.text = "The app has became active \(didBecomeActiveCount) time(s)"
+        willTerminateLabel.text = "The app has terminated \(willTerminateCount) time(s)"
+    }
 }
 
