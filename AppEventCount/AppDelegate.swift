0c75d959d49a55e569a15bd6313c63033e93ceb9
Ahmed Naser <201700099@student.polytechnic.bh>
2020-11-20 17:04:41 +0300

    Switch to iOS 12 project

--- AppEventCount/AppDelegate.swift
+++ AppEventCount/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow? // was not here in the 1st place, I added it manually but still didn't work
@@ -15,27 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-    
-    func applicationDidFinishLaunching(_ application: UIApplication) {
         viewController = window?.rootViewController as? ViewController // Won't compile since window not declared in the scope
-        
         viewController?.didFinishLaunchingCount += 1
+
+        return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
